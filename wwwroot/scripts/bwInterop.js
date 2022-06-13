748208441f36e03f4b13e37e84f47859108cf6f8
Joaquin Noe@ <jonoe96@protonmail.com>
2022-06-13 14:17:55 -0300

    FEAT: Add C# App

---
+++ wwwroot/scripts/bwInterop.js
@@ -0,0 +1,20 @@
+﻿var bweInterop = {};
+
+bweInterop.getPosition = async function () {
+    function getPositionAsync() {
+        return new Promise((success, err) => {
+            navigator.geolocation
+                .getCurrentPosition(success, err);
+        });
+    }
+
+    if (navigator.geolocation) {
+        let position = await getPositionAsync();
+        return {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        };
+    } else {
+        throw Error("Geolocation is not supported by this browser.");
+    }
+}
